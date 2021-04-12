c9a3a8b25ba23ea114331abd70791100f98e9203
motty93 <rdwbocungelt5@gmail.com>
2021-04-12 22:30:13 +0900

    feat: router

--- router/src/router.js
+++ router/src/router.js
@@ -2,9 +2,12 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Users from './views/Users.vue'
 
+const routes = [
+  { path: '/', component: Home },
+  { path: '/users', component: Users }
+]
+
 export default new Router({
-  routes: [
-    { path: '/', component: Home },
-    { path: '/users', component: Users }
-  ]
+  // routes: routesの短縮表記
+  routes
 })
